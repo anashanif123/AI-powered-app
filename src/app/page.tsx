a03e10596508fe8eacd9b900a5b4de7a647c6e0b
MUHAMMAD ANAS <154446804+anashanif123@users.noreply.github.com>
2025-07-09 23:41:07 +0500

    database setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import { Button } from "@/components/ui/button";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-      <p className="mt-4 text-lg">
-        This is a simple Next.js application with Geist fonts.
-        <Button variant={"destructive"}>
-          Click Me
-        </Button>
-      </p>
-    </main>
-  );
+
+ const Page =async()=> {
+
+
+  return(
+    <div>
+      <h1>nextjs</h1>
+    </div>
+  )
+  
 }
+export default Page;
