b6681596b761939bccceb609314e7820df02999e
MUHAMMAD ANAS <154446804+anashanif123@users.noreply.github.com>
2025-07-12 22:54:59 +0500

    check api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 const Page =  () => {
   const trpc = useTRPC();
   trpc.hello.queryOptions({ text: "hello world" });
+  const {data} =useQuery(trpc.hello.queryOptions({ text: "Anas" }));
   return (
     <div>
-      <h1>nextjs</h1>
+      <h1>{JSON.stringify(data)}</h1>
     </div>
   );
 };
